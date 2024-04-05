1adeae5085e9b9362453649ae3ff2d5c4340411f
Chinedum Echeta <60179183+cecheta@users.noreply.github.com>
2024-04-05 15:41:54 +0000

    Add httpx instrumentation

--- code/app.py
+++ code/app.py
@@ -2,6 +2,11 @@ from azure.monitor.opentelemetry import configure_azure_monitor
 
 configure_azure_monitor()
 
+# httpx is used by openai
+from opentelemetry.instrumentation.httpx import HTTPXClientInstrumentor  # noqa: E402
+
+HTTPXClientInstrumentor().instrument()
+
 from create_app import create_app  # noqa: E402
 
 app = create_app()
